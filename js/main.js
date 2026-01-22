b91812e3267de9aa66c364b0eda9dddf127a9a68
Michele-Brignola <michele.brignola@hotmail.com>
2026-01-22 17:04:55 +0100

    added email list

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
-function generateTeamCard(member) {
-  const { name, role, email, img } = member;
+const list = document.getElementById("email-list");
 
-  const teamCard = `
-    <div class="card-team col">
-      <div class="d-flex bg-black">
-        <div class="col-4 img-card">
-          <img class="img-fluid" src="./${img}" alt="${name}" />
-        </div>
-        <div class="text-card text-white p-3 col-8">
-          <h3>${name}</h3>
-          <p>${role}</p>
-          <a href="mailto:${email}">${email}</a>
-        </div>
-      </div>
-    </div>`;
-
-  return teamCard;
+for (let i = 0; i < 10; i++) {
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response) => {
+      const email = response.data.response;
+      list.innerHTML += `<li>${email}</li>`
+    })
+    .catch((error) => {
+      console.error(error);
+      list.innerHTML += `Error: ${error}`
+    });
 }
-
-function printCards() {
-  const list = document.getElementById("email-list")
-
-  for (const member of teamMembers) {
-    containerElement.innerHTML += generateTeamCard(member);
-  }
-}
-
-printCards();
